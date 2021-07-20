773fb3115b258ca10db191196a57c1674a0ce939
NguyenHulob <87717855+NguyenHulob@users.noreply.github.com>
2021-07-21 00:53:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
+    introTitle: 'Linh à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Phải chăng tớ đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng tớ đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích tớ nhiều <33',
+    btnNo: 'Không, cậu bị ảo tưởng à :3',
+    question:'Trái tim đỏ mà tớ thả vào bài viết của cậu í, không phải cho hình hay cap mà cậu đăng đâu, là cho cậu đó <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Thương thì thương mà không thương buộc thương <33333333',
+    mess: 'Tớ biết mà 🥰.Yêu cậu 😘😘',
+    messDesc: 'Thôi, nghịch cậu đủ rồi, giờ cậu hãy vào nhắn tin với tớ đi.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100010913476138' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
