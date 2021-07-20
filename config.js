4baaa68afd7f4e2cf498ea9259776e30c67913dc
NguyenHulob <87717855+NguyenHulob@users.noreply.github.com>
2021-07-21 00:26:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Ngân thân mến à!',
+    introDesc: `Ủa đêm rồi mà sao tim mình vẫn đầy nắng thế?`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Cậu cảm thấy như nào khi cậu là cô gái dễ thương nhất ở đây? 😙',
+    desc: ' Tớ ngồi ở đây khoảng 5 phút đợi cậu rep tin nhắn mà tớ chưa thấy cô gái nào rep tin nhắn dễ thương như cậu cả. ',
+    btnYes: 'Tớ biết cậu thích tớ mà <33',
+    btnNo: 'Không, làm gì có chuyện đó bạn thân à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao tớ lại thương cậu nhỉ <3',
+    btnReply: 'Gửi cho tớ vài dòng tin nhắn đi <3',
+    reply: 'Thương thì thương mà không thương buộc thương <33333333',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Nói thế thôi chứ có người yêu nhanh đi không ta hóng mòn hết khá nhiều dép rồi đấy. Giờ thì qua nhắn tin với ta đi.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/nguyen.nguyentrang.169' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
