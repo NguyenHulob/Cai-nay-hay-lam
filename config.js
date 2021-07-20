521072fd7ea6c42a73ca87f744e6765aa1867876
NguyenHulob <87717855+NguyenHulob@users.noreply.github.com>
2021-07-21 00:15:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Ngân thân thương!',
+    introDesc: `Ủa đêm rồi mà sao tim mình vẫn đầy nắng thế?`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'Phải chăng tao đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng tao đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Tao biết Ngân thương tao mà <33',
+    btnNo: 'Không, Người bạn thân à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao ta lại thương mày ta <3',
+    btnReply: 'Gửi cho tao vài lời nhắn đi <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Ta biết mày thương ta mà 😘😘',
+    messDesc: 'Có người yêu nhớ khao bạn nháa, giờ thì hay inbox cho tao nhanh đi.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/nguyen.nguyentrang.169/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
